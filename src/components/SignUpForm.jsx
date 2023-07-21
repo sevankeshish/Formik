1239e26e4954de85901aaf5b347b52aaf968571e
sevan.keshish0 <sevan.keshish0@gmail.com>
2023-07-21 12:31:02 +0400

    adding onChange and values

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,18 +1,28 @@
+import { useState } from "react"
+
 const SignUpForm = () => {
+    const [userData , setUserData] = useState({name:"", email:"", password:""})
+
+    //let destructure it (e.target.value)
+    const changeHandler = ({target}) =>{
+        console.log(target);
+        setUserData({...userData, [target.name] : target.value})
+    }
+
     return(
         <div>
             <form>
                 <div className="formControl">
                     <label>Name</label>
-                    <input type="text" />
+                    <input type="text" name="name" value={userData.name} onChange={changeHandler} />
                 </div>
                 <div className="formControl">
                     <label>Email</label>
-                    <input type="text" />
+                    <input type="text" name="email" value={userData.email} onChange={changeHandler} />
                 </div>
                 <div className="formControl">
                     <label>Password</label>
-                    <input type="text" />
+                    <input type="text" name="password" value={userData.password} onChange={changeHandler} />
                 </div>
                 <button>submit</button>
             </form>
